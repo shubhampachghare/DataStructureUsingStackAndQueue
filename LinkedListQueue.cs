86d89ba8a1f3495c6bdf7171ba6443dcb366f8da
shubhampachghare <shubhampachghare@gmail.com>
2022-12-15 16:51:04 +0530

    [Shubham]-EnQueue method for insert data into queue

---
+++ LinkedListQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureUsingStackAndQueue
+{
+    public  class LinkedListQueue
+    {
+        Node head;
+        public LinkedListQueue()
+        {
+            this.head = null;
+        }
+
+        public void EnQueue(int data)
+        {
+
+
+            Node node = new Node(data);
+            if (head == null)
+            {
+
+                head = node;
+            }
+            else
+            {
+
+                Node temp = head;
+
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = node;
+                // this.head = temp;
+                
+                
+
+            }
+        }
+        public void display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is empty");
+
+            }
+            else
+            {
+                Node temp = head;
+
+                while (temp != null && temp.data != null)
+                {
+                    Console.WriteLine("Inserted into the Queue Successfully = " + temp.data);
+                    temp = temp.next;
+
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace DataStructureUsingStackAndQueue
             linkedListStack.push(56);
             linkedListStack.Display();*/
 
-            LinkedListStack linkedListStack1 = new LinkedListStack();
+            /*LinkedListStack linkedListStack1 = new LinkedListStack();
             linkedListStack1.push(70);
             linkedListStack1.push(30);
             linkedListStack1.push(56);
@@ -24,6 +24,13 @@ namespace DataStructureUsingStackAndQueue
             linkedListStack1.Display();
             linkedListStack1.isEmpty();
             linkedListStack1.Display();
+            */
+
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.EnQueue(56);
+            queue.EnQueue(30);
+            queue.EnQueue(70);
+            queue.display();
         }
     }
 }
