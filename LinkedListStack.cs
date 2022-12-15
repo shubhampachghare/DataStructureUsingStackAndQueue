ac6c3766ae76bd9f3fc2222f3fdd00a055192095
shubhampachghare <shubhampachghare@gmail.com>
2022-12-15 16:00:16 +0530

    [Shubham]-Push method for insert data into stack

---
+++ LinkedListStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataStructureUsingStackAndQueue
+{
+    class LinkedListStack
+    {
+        public Node top;
+
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        
+        public void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(value);
+        }
+
+       
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("Inserted into the stack Successfully = " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace DataStructureUsingStackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome datastructure using stack and queue");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
         }
     }
 }
